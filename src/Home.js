ab12141a6e1da7a90acc96c80c8106ac5ee45e88
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-08-20 12:39:39 -0500

    Module 5 completed

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+class Home extends Component {
+
+  render() {
+    const {isAuthenticated, login} = this.props.auth;
+    return (
+      <div>
+        <h1>Home</h1>
+        { isAuthenticated() ? <Link to="/profile">View Profile</Link> : <button onClick={login}>Log In</button>}
+      </div>
+    );
+  }
 };
 
 export default Home;
