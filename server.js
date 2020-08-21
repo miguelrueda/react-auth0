06a506075bf72205c8053bd773dd661d0cd990bc
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-08-21 13:13:17 -0500

    Module 8 finished

--- server.js
+++ server.js
@@ -30,12 +30,29 @@ app.get("/private", checkJwt, function(req, res) {
     });
 });
 
+function checkRole(role) {
+    return function(req, res, next) {
+        const assignedRoles = req.user["http://localhost:3000/roles"];
+        if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+            return next();
+        } else {
+            return res.status(401).send("Insufficient role");
+        }
+    }
+}
+
 app.get("/courses", checkJwt, checkScope(["read:courses"]), function(req, res) {
     res.json({
         courses: [
             {id: 1, title: "Title 1"},
             {id: 2, title: "Title 2"},
         ]
+    });
+});
+
+app.get("/admin", checkJwt, checkRole('admin'), function(req, res) {
+    res.json({
+        message: "Hello from an admin API!"
     });
 });
 
